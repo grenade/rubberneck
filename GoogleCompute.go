2f97b390a5f08d68b2ed546cee1f9508630d0207
Rob Thijssen <rthijssen@gmail.com>
2019-04-23 14:49:47 +0300

    observe taskcluster worker state across gcp zones

--- GoogleCompute.go
+++ GoogleCompute.go
@@ -11,9 +11,9 @@ import (
 )
 
 func GoogleCompute() {
-	projects := [...]string{
-		"windows-workers",
-	}
+  projects := [...]string{
+    "windows-workers",
+  }
 
   ctx := context.Background()
   client, err := google.DefaultClient(ctx,compute.ComputeScope)
@@ -22,31 +22,31 @@ func GoogleCompute() {
   }
   computeService, err := compute.New(client)
   for _, project := range projects {
-	  zoneListCall := computeService.Zones.List(project)
-	  zoneList, err := zoneListCall.Do()
-	  if err != nil {
-	    fmt.Println("Error", err)
-	  } else {
-	    for _, zone := range zoneList.Items {
-		    instanceListCall := computeService.Instances.List(project, zone.Name)
-		    instanceList, err := instanceListCall.Do()
-		    if err != nil {
-		      fmt.Println("Error", err)
-		    } else {
-		      for _, instance := range instanceList.Items {
-		        if workerType, isWorker := instance.Labels["worker-type"]; isWorker {
-		          m := strings.Split(instance.MachineType, "/")
-		          fmt.Printf("cloud: gcp, zone: %v, name: %v, instance id: %v, machine type: %v, worker type: %v, launch time: %v\n",
-		            zone.Name,
-		            instance.Name,
-		            instance.Id,
-		            m[len(m)-1],
-		            workerType,
-		            instance.CreationTimestamp)
-		        }
-		      }
-		    }
-		  }
-	  }
-	}
+    zoneListCall := computeService.Zones.List(project)
+    zoneList, err := zoneListCall.Do()
+    if err != nil {
+      fmt.Println("Error", err)
+    } else {
+      for _, zone := range zoneList.Items {
+        instanceListCall := computeService.Instances.List(project, zone.Name)
+        instanceList, err := instanceListCall.Do()
+        if err != nil {
+          fmt.Println("Error", err)
+        } else {
+          for _, instance := range instanceList.Items {
+            if workerType, isWorker := instance.Labels["worker-type"]; isWorker {
+              m := strings.Split(instance.MachineType, "/")
+              fmt.Printf("cloud: gcp, zone: %v, name: %v, instance id: %v, machine type: %v, worker type: %v, launch time: %v\n",
+                zone.Name,
+                instance.Name,
+                instance.Id,
+                m[len(m)-1],
+                workerType,
+                instance.CreationTimestamp)
+            }
+          }
+        }
+      }
+    }
+  }
 }
