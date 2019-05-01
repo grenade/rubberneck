c95d0c11724d1b7446cfa354bd36e7462a65972c
Rob Thijssen <rthijssen@gmail.com>
2019-05-01 10:44:35 +0300

    Updates

---
+++ precache-manifest.3f7f08de9fb6b37f178a0e1ae04070b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4924e24e3d19213bc180554cf54cc0",
+    "url": "/rubberneck/index.html"
+  },
+  {
+    "revision": "5fbf290f169e9cc3d298",
+    "url": "/rubberneck/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rubberneck/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5fbf290f169e9cc3d298",
+    "url": "/rubberneck/static/js/main.af759920.chunk.js"
+  },
+  {
+    "revision": "99e842052096534fb1ae",
+    "url": "/rubberneck/static/js/runtime~main.050fed21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rubberneck/static/media/logo.5d5d9eef.svg"
+  }
+]);
