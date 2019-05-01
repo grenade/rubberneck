11e25520e5e4fa1b7a1e83f176af9d001caf77da
Rob Thijssen <rthijssen@gmail.com>
2019-05-01 17:01:26 +0300

    sort by os

--- src/components/workerTypeCounts.js
+++ src/components/workerTypeCounts.js
@@ -19,61 +19,67 @@ const WorkerTypeCounts = ({ workerTypeCounts }) => {
           <FontAwesomeIcon icon={['fab', 'windows']} />
         </button>
       </div>
-      <div className="row">
-        {Object.keys(workerTypeCounts).map((workerType, i) => (
-          <div className="card" key={i}>
-            <div className="card-body">
-              <center>
-                <h3 className="card-title">
-                  {
-                    workerType.includes("osx")
-                      ? (<FontAwesomeIcon icon={['fab', 'apple']} />)
-                      : workerType.includes("linux")
-                        ? (<FontAwesomeIcon icon={['fab', 'linux']} />)
-                        : workerType.includes("win")
-                          ? (<FontAwesomeIcon icon={['fab', 'windows']} />)
-                          : workerType.includes("android")
-                            ? (<FontAwesomeIcon icon={['fab', 'android']} />)
-                            : (<FontAwesomeIcon icon={['fas', 'desktop']} />)
-                  }
-                </h3>
-              </center>
-              <h5 className="card-title">
-                {workerType}
-              </h5>
-              <hr />
-              <h6 className="card-title">
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'server']} />
-                instances
-              </h6>
-              <p className="card-text">
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'power-off']} />
-                pending: {workerTypeCounts[workerType].pending}<br />
+        {
+          ['android', 'osx', 'linux', 'win'].map((os, osIndex) => (
+            <div className="row" key={osIndex}>
+            {
+              Object.keys(workerTypeCounts).filter(workerType => { return workerType.includes(os); }).map((workerType, workerTypeIndex) => (
+                <div className="card" key={workerTypeIndex}>
+                  <div className="card-body">
+                    <center>
+                      <h3 className="card-title">
+                        {
+                          workerType.includes("osx")
+                            ? (<FontAwesomeIcon icon={['fab', 'apple']} />)
+                            : workerType.includes("linux")
+                              ? (<FontAwesomeIcon icon={['fab', 'linux']} />)
+                              : workerType.includes("win")
+                                ? (<FontAwesomeIcon icon={['fab', 'windows']} />)
+                                : workerType.includes("android")
+                                  ? (<FontAwesomeIcon icon={['fab', 'android']} />)
+                                  : (<FontAwesomeIcon icon={['fas', 'desktop']} />)
+                        }
+                      </h3>
+                    </center>
+                    <h5 className="card-title">
+                      {workerType}
+                    </h5>
+                    <hr />
+                    <h6 className="card-title">
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'server']} />
+                      instances
+                    </h6>
+                    <p className="card-text">
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'power-off']} />
+                      pending: {workerTypeCounts[workerType].pending}<br />
 
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'recycle']} />
-                waiting: {workerTypeCounts[workerType].waiting}<br />
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'recycle']} />
+                      waiting: {workerTypeCounts[workerType].waiting}<br />
 
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'wrench']} />
-                working: {workerTypeCounts[workerType].working}
-              </p>
-              <ProgressBar>
-                <ProgressBar striped variant="success" now={workerTypeCounts[workerType].working / workerTypeCounts[workerType].running * 100} key={1} />
-                <ProgressBar variant="warning" now={workerTypeCounts[workerType].waiting / workerTypeCounts[workerType].running * 100} key={2} />
-                <ProgressBar striped variant="danger" now={workerTypeCounts[workerType].pending / workerTypeCounts[workerType].running * 100} key={3} />
-              </ProgressBar>
-              <hr />
-              <h6 className="card-title">
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'tasks']} />
-                tasks
-              </h6>
-              <p className="card-text">
-                <FontAwesomeIcon className="text-muted" icon={['fas', 'clock']} />
-                pending: 0
-              </p>
-            </div>
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'wrench']} />
+                      working: {workerTypeCounts[workerType].working}
+                    </p>
+                    <ProgressBar>
+                      <ProgressBar striped variant="success" now={workerTypeCounts[workerType].working / workerTypeCounts[workerType].running * 100} key={1} />
+                      <ProgressBar variant="warning" now={workerTypeCounts[workerType].waiting / workerTypeCounts[workerType].running * 100} key={2} />
+                      <ProgressBar striped variant="danger" now={workerTypeCounts[workerType].pending / workerTypeCounts[workerType].running * 100} key={3} />
+                    </ProgressBar>
+                    <hr />
+                    <h6 className="card-title">
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'tasks']} />
+                      tasks
+                    </h6>
+                    <p className="card-text">
+                      <FontAwesomeIcon className="text-muted" icon={['fas', 'clock']} />
+                      pending: 0
+                    </p>
+                  </div>
+                </div>
+              ))
+            }
           </div>
-        ))}
-      </div>
+        ))
+      }
     </div>
   )
 };
