908bd50ea6f25a81f6e0f644db1930227eb92687
Rob Thijssen <rthijssen@gmail.com>
2019-05-01 16:31:42 +0300

    visualise counts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import WorkerTypeCounts from './components/workerTypeCounts';
-//import './App.css';
+
+import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDesktop, faRecycle, faWrench, faPowerOff, faClock, faServer, faTasks } from '@fortawesome/free-solid-svg-icons'
+import { faAndroid, faApple, faLinux, faWindows } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(
+  faTasks,
+  faServer,
+  faClock,
+  faDesktop,
+  faRecycle,
+  faWrench,
+  faPowerOff,
+  faAndroid,
+  faApple,
+  faLinux,
+  faWindows
+)
 
 class App extends Component {
-  componentDidMount() {
-    this.fetch('https://raw.githubusercontent.com/grenade/rubberneck/observe/worker-type-counts.json')
+  constructor() {
+    super();
+    this.state = {
+      workerTypeCounts: {}
+    };
+    fetch('https://raw.githubusercontent.com/grenade/rubberneck/observe/worker-type-counts.json')
     .then(res => res.json())
     .then((data) => {
       this.setState({ workerTypeCounts: data })
     })
-    .catch(console.log)
+    .catch(console.log);
   }
   render () {
     return (
--- src/components/workerTypeCounts.js
+++ src/components/workerTypeCounts.js
@@ -1,26 +1,79 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const WorkerTypeCounts = ({ workerTypeCounts }) => {
   return (
-    <div>
-      <center><h1>Worker Type List</h1></center>
-      {Object.keys(workerTypeCounts).map((workerType) => (
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">{workerType}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">blah, blah</h6>
-            <p class="card-text">blah</p>
-            <dl>
-              <dt>pending</dt>
-              <dd>{workerTypeCounts[workerType].pending}</dd>
-              <dt>waiting</dt>
-              <dd>{workerTypeCounts[workerType].waiting}</dd>
-              <dt>working</dt>
-              <dd>{workerTypeCounts[workerType].working}</dd>
-            </dl>
+    <div className="container">
+      <div className="row">
+        <button type="button" className="btn btn-outline-secondary">
+          <FontAwesomeIcon icon={['fab', 'apple']} />
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <FontAwesomeIcon icon={['fab', 'android']} />
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <FontAwesomeIcon icon={['fab', 'linux']} />
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <FontAwesomeIcon icon={['fab', 'windows']} />
+        </button>
+      </div>
+      <div className="row">
+        {Object.keys(workerTypeCounts).map((workerType, i) => (
+          <div className="card" key={i}>
+            <div className="card-body">
+              <center>
+                <h3 className="card-title">
+                  {
+                    workerType.includes("osx")
+                      ? (<FontAwesomeIcon icon={['fab', 'apple']} />)
+                      : workerType.includes("linux")
+                        ? (<FontAwesomeIcon icon={['fab', 'linux']} />)
+                        : workerType.includes("win")
+                          ? (<FontAwesomeIcon icon={['fab', 'windows']} />)
+                          : workerType.includes("android")
+                            ? (<FontAwesomeIcon icon={['fab', 'android']} />)
+                            : (<FontAwesomeIcon icon={['fas', 'desktop']} />)
+                  }
+                </h3>
+              </center>
+              <h5 className="card-title">
+                {workerType}
+              </h5>
+              <hr />
+              <h6 className="card-title">
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'server']} />
+                instances
+              </h6>
+              <p className="card-text">
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'power-off']} />
+                pending: {workerTypeCounts[workerType].pending}<br />
+
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'recycle']} />
+                waiting: {workerTypeCounts[workerType].waiting}<br />
+
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'wrench']} />
+                working: {workerTypeCounts[workerType].working}
+              </p>
+              <ProgressBar>
+                <ProgressBar striped variant="success" now={workerTypeCounts[workerType].working / workerTypeCounts[workerType].running * 100} key={1} />
+                <ProgressBar variant="warning" now={workerTypeCounts[workerType].waiting / workerTypeCounts[workerType].running * 100} key={2} />
+                <ProgressBar striped variant="danger" now={workerTypeCounts[workerType].pending / workerTypeCounts[workerType].running * 100} key={3} />
+              </ProgressBar>
+              <hr />
+              <h6 className="card-title">
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'tasks']} />
+                tasks
+              </h6>
+              <p className="card-text">
+                <FontAwesomeIcon className="text-muted" icon={['fas', 'clock']} />
+                pending: 0
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 };
