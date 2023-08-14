28aab775796584ce383c9758209195cf1eb6cbfe
Rob Thijssen <rthijssen@gmail.com>
2023-08-14 12:07:20 +0300

    debug: try auto cypher selection

--- manifest/miyamoto.pelagos.systems/beta/etc/mysql/init/change-replication-source.sql
+++ manifest/miyamoto.pelagos.systems/beta/etc/mysql/init/change-replication-source.sql
@@ -6,7 +6,6 @@ CHANGE REPLICATION SOURCE TO
   SOURCE_SSL_CA                 = '/etc/ssl/chain.pem',
   SOURCE_SSL_CERT               = '/etc/ssl/cert.pem',
   SOURCE_SSL_KEY                = '/etc/ssl/key.pem',
-  SOURCE_SSL_CIPHER             = 'TLS_AES_256_GCM_SHA384',
   SOURCE_SSL_VERIFY_SERVER_CERT = 1,
   SOURCE_TLS_VERSION            = 'TLSv1.3',
   SOURCE_AUTO_POSITION          = 1;
