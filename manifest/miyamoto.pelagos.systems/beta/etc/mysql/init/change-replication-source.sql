a9b496d5815b96c3307aefd470f537f64ed7ebcd
Rob Thijssen <rthijssen@gmail.com>
2023-08-15 11:59:15 +0300

    set tls ciphersuites

--- manifest/miyamoto.pelagos.systems/beta/etc/mysql/init/change-replication-source.sql
+++ manifest/miyamoto.pelagos.systems/beta/etc/mysql/init/change-replication-source.sql
@@ -4,10 +4,12 @@ CHANGE REPLICATION SOURCE TO
   SOURCE_HOST                   = 'alpha.miyamoto.pelagos.systems',
   SOURCE_PORT                   = 3306,
   SOURCE_USER                   = 'beta',
+  SOURCE_AUTO_POSITION          = 1,
   SOURCE_SSL                    = 1,
+  SOURCE_SSL_VERIFY_SERVER_CERT = 1,
   SOURCE_SSL_CA                 = '/var/lib/mysql/lets-encrypt-ca.pem',
   SOURCE_SSL_CERT               = '/var/lib/mysql/lets-encrypt-cert.pem',
   SOURCE_SSL_KEY                = '/var/lib/mysql/lets-encrypt-key.pem',
-  SOURCE_SSL_VERIFY_SERVER_CERT = 1,
-  SOURCE_AUTO_POSITION          = 1;
+  SOURCE_TLS_VERSION            = 'TLSv1.3',
+  SOURCE_TLS_CIPHERSUITES       = 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256';
 START REPLICA;
