d6e8b178cdcb20cc6b1ef1d7cb6920c70172b41c
rob thijssen <rthijssen@gmail.com>
2023-10-17 13:42:32 +0400

    configure gitlab

---
+++ manifest/thgttg.com/marvin/etc/gitlab/gitlab.rb
@@ -0,0 +1,10 @@
+## gitlab
+external_url "https://gitlab.thgttg.com"
+letsencrypt['contact_emails'] = ['ops@thgttg.com']
+
+## container registry
+registry_external_url "https://registry.thgttg.com"
+
+## mattermost
+mattermost_external_url "https://mattermost.thgttg.com"
+mattermost_nginx['redirect_http_to_https'] = true
