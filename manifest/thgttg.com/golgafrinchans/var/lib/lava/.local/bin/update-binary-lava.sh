c36705bc9e550db58df259cef91b48729580c356
Rob Thijssen <rthijssen@gmail.com>
2024-03-01 17:25:34 +0200

    lava app config

--- manifest/thgttg.com/golgafrinchans/var/lib/lava/.local/bin/update-binary-lava.sh
+++ manifest/thgttg.com/golgafrinchans/var/lib/lava/.local/bin/update-binary-lava.sh
@@ -8,6 +8,8 @@ latest_tag=$(curl \
   | jq -r '.[0].name')
 binary_path=/var/lib/lava/.local/bin/lavad
 binary_url=https://github.com/${repo}/releases/download/${latest_tag}/lavad-${latest_tag}-linux-amd64
+config_path=/var/lib/lava/.lava/config
+config_url=https://github.com/lavanet/lava-config/raw/main/testnet-2/default_lavad_config_files
 
 [ -d $(dirname ${binary_path}) ] || mkdir -p $(dirname ${binary_path})
 
@@ -25,3 +27,18 @@ else
   echo "failed to download ${binary_path} version ${latest_tag:1} from ${binary_url}"
   exit 1
 fi
+
+for toml in {app,client,config}.toml; do
+  path=${config_path}/${toml}
+  url=${config_url}/${toml}
+  if curl \
+    --silent \
+    --location \
+    --output ${path} \
+    --url ${url}; then
+    echo "${path} downloaded from ${url}"
+  else
+    echo "failed to download ${path} from ${url}"
+    exit 1
+  fi
+done
