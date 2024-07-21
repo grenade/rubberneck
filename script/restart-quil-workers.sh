f65849f1f57db40bc27512b29d02d5747862e063
rob thijssen <rthijssen@gmail.com>
2024-07-21 15:54:35 +0300

    quil restarter

--- script/restart-quil-workers.sh
+++ script/restart-quil-workers.sh
@@ -1,22 +1,67 @@
 #!/usr/bin/env bash
 
-echo allitnils.thgttg.com
-ssh allitnils.thgttg.com 'for core in {25..36}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)quordlepleen.thgttg.com$(tput dim)"
+ssh quordlepleen.thgttg.com '
+  echo "  - quilibrium.service: $(systemctl is-active quilibrium.service) $(systemctl is-enabled quilibrium.service)";
+  systemctl is-active --quiet quilibrium.service || sudo systemctl restart quilibrium.service;
+  systemctl is-enabled --quiet quilibrium.service || sudo systemctl enable quilibrium.service;
+  for core in {1..24}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo gramathea.thgttg.com
-ssh gramathea.thgttg.com 'for core in {37..96}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)allitnils.thgttg.com$(tput dim)"
+ssh allitnils.thgttg.com '
+  for core in {25..36}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo hawalius.thgttg.com
-ssh hawalius.thgttg.com 'for core in {97..114}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)gramathea.thgttg.com$(tput dim)"
+ssh gramathea.thgttg.com '
+  for core in {37..96}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo krikkit.thgttg.com
-ssh krikkit.thgttg.com 'for core in {115..132}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)hawalius.thgttg.com$(tput dim)"
+ssh hawalius.thgttg.com '
+  for core in {97..114}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo quordlepleen.thgttg.com
-ssh quordlepleen.thgttg.com 'for core in {1..24}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)krikkit.thgttg.com$(tput dim)"
+ssh krikkit.thgttg.com '
+  for core in {115..132}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo slartibartfast.thgttg.com
-ssh slartibartfast.thgttg.com 'for core in {193..204}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)midgard.v8r.io$(tput dim)"
+ssh midgard.v8r.io '
+  for core in {133..192}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
 
-echo midgard.v8r.io
-ssh midgard.v8r.io 'for core in {133..192}; do systemctl is-active quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service; done'
+echo "- $(tput bold)slartibartfast.thgttg.com$(tput dim)"
+ssh slartibartfast.thgttg.com '
+  for core in {193..204}; do
+    echo "  - quilibrium-worker@${core}.service: $(systemctl is-active quilibrium-worker@${core}.service) $(systemctl is-enabled quilibrium-worker@${core}.service)";
+    systemctl is-active --quiet quilibrium-worker@${core}.service || sudo systemctl restart quilibrium-worker@${core}.service;
+  done
+'
+echo $(tput sgr0)
