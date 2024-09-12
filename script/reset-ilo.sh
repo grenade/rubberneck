7b20db55a54e374acf637e4a1d2a9be3eb2f8af8
rob thijssen <rthijssen@gmail.com>
2024-09-12 13:22:27 +0300

    ilo stuff

--- script/reset-ilo.sh
+++ script/reset-ilo.sh
@@ -14,7 +14,6 @@ fqdn_list+=( golgafrinchans.thgttg.com )
 fqdn_list+=( gramathea.thgttg.com )
 fqdn_list+=( hawalius.thgttg.com )
 fqdn_list+=( krikkit.thgttg.com )
-fqdn_list+=( midgard.thgttg.com )
 fqdn_list+=( mitko.thgttg.com )
 fqdn_list+=( novgorodian.thgttg.com )
 fqdn_list+=( quordlepleen.thgttg.com )
@@ -23,16 +22,19 @@ fqdn_list+=( midgard.v8r.io )
 
 for fqdn in ${fqdn_list[@]}; do
   hostname=${fqdn%%.*}
-  observed_hostname=$(ssh ${hostname} "
-    [ -x /usr/sbin/hponcfg ] || sudo dnf install -y https://downloads.hpe.com/pub/softlib2/software1/pubsw-linux/p215998034/v109045/hponcfg-4.6.0-0.x86_64.rpm &> /dev/null;
-    echo '<RIBCL VERSION=\"2.0\"><LOGIN USER_LOGIN=\"Administrator\" PASSWORD=\"\"><SERVER_INFO MODE=\"read\"><GET_SERVER_NAME /></SERVER_INFO></LOGIN></RIBCL>' | sudo hponcfg --input | grep SERVER_NAME | cut -d '\"' -f 2
+  #observed_hostname=$(ssh ${hostname} "
+  #  [ -x /usr/sbin/hponcfg ] || sudo dnf install -y https://downloads.hpe.com/pub/softlib2/software1/pubsw-linux/p215998034/v109045/hponcfg-4.6.0-0.x86_64.rpm &> /dev/null;
+  #  echo '<RIBCL VERSION=\"2.0\"><LOGIN USER_LOGIN=\"Administrator\" PASSWORD=\"\"><SERVER_INFO MODE=\"read\"><GET_SERVER_NAME /></SERVER_INFO></LOGIN></RIBCL>' | sudo hponcfg --input | grep SERVER_NAME | cut -d '\"' -f 2
+  #")
+  observed_fqdn=$(ssh ${hostname} "
+    echo '<RIBCL VERSION=\"2.0\"><LOGIN USER_LOGIN=\"Administrator\" PASSWORD=\"\"><SERVER_INFO MODE=\"read\"><GET_SERVER_FQDN /></SERVER_INFO></LOGIN></RIBCL>' | sudo hponcfg --input | grep SERVER_FQDN | cut -d '\"' -f 2
   ")
-  if [ "${hostname}" = "${observed_hostname}" ]; then
-    echo "- ${fqdn}: ✅"
+  if [ "${fqdn}" = "${observed_fqdn}" ]; then
+    echo "✅ ${fqdn}"
   else
     ssh ${hostname} "
-      echo '<RIBCL VERSION=\"2.0\"><LOGIN USER_LOGIN=\"Administrator\" PASSWORD=\"\"><SERVER_INFO MODE=\"write\"><SERVER_NAME value=\"${hostname}\"/></SERVER_INFO></LOGIN></RIBCL>' | sudo hponcfg --input &> /dev/null
+      echo '<RIBCL VERSION=\"2.0\"><LOGIN USER_LOGIN=\"Administrator\" PASSWORD=\"\"><SERVER_INFO MODE=\"write\"><SERVER_FQDN value=\"${fqdn}\"/></SERVER_INFO></LOGIN></RIBCL>' | sudo hponcfg --input &> /dev/null
     "
-    echo "- ${fqdn}: 🔨 (${observed_hostname} -> ${hostname})"
+    echo "🔨 ${fqdn}: ${observed_fqdn} -> ${fqdn}"
   fi
 done
