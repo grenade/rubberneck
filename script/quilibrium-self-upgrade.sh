ce795e81f7349143602d21fbf0afe2f17bdf1286
rob thijssen <rthijssen@gmail.com>
2024-11-29 11:23:23 +0200

    logic corrections

--- script/quilibrium-self-upgrade.sh
+++ script/quilibrium-self-upgrade.sh
@@ -5,43 +5,58 @@ if ((EUID != 0)); then
   exit 1
 fi
 
+#sudo setenforce 0
+#sudo setenforce 1
+#sudo systemctl stop quilibrium.service
+for sig in ${quilibrium_home}/.local/bin/quilibrium.dgst.sig.{1..20}; do
+    if sudo -u ${quilibrium_user} test -e ${sig}; then
+        sudo -u ${quilibrium_user} rm ${sig};
+    fi
+done;
 quilibrium_home=/var/lib/quilibrium
 quilibrium_user=quilibrium
 remote_artifacts=($(curl -sL https://releases.quilibrium.com/release | grep linux-amd64))
 latest_version=$(echo ${remote_artifacts[0]} | cut -d \- -f 2)
-for sig in ${quilibrium_home}/.local/bin/quilibrium.dgst.sig.{1..20}; do
-    if [ -e ${sig} ]; then
-        rm ${sig};
-    fi
-done;
 for remote_artifact in ${remote_artifacts[@]}; do
     remote_artifact_url=https://releases.quilibrium.com/${remote_artifact}
     local_artifact_path=${quilibrium_home}/.local/bin/${remote_artifact}
     local_artifact_link=${quilibrium_home}/.local/bin/${remote_artifact/node-${latest_version}-linux-amd64/quilibrium}
-    if [ ! -s ${local_artifact_path} ]; then
-    	if sudo -u ${quilibrium_user} /usr/bin/curl \
+    if sudo -u ${quilibrium_user} test -s ${local_artifact_path}; then
+        echo "auto-updater: observed ${local_artifact_path}"
+    else
+        if sudo -u ${quilibrium_user} /usr/bin/curl \
             --fail \
-            --location \
             --silent \
+            --location \
             --output ${local_artifact_path} \
             --url ${remote_artifact_url} \
-            && [ -s ${local_artifact_path} ]; then
+            && sudo -u ${quilibrium_user} test -s ${local_artifact_path}; then
             echo "auto-updater: downloaded ${local_artifact_path} from ${remote_artifact_url}"
         else
             echo "auto-updater: failed to download ${local_artifact_path} from ${remote_artifact_url}"
         fi
     fi
-    sudo -u ${quilibrium_user} /usr/bin/ln -sfr ${local_artifact_path} ${local_artifact_link}
-    if [ "${remote_artifact}" = "node-${latest_version}-linux-amd64" ] && [ ! -x ${local_artifact_path} ]; then
-        chmod +x ${local_artifact_path}
-        semanage fcontext -a -t bin_t ${local_artifact_path}
-        chcon -v -u system_u -t bin_t ${local_artifact_path}
-        semanage fcontext -a -t bin_t ${local_artifact_link}
-        chcon -v -u system_u -t bin_t ${local_artifact_link}
+    if sudo -u ${quilibrium_user} test -s ${local_artifact_path}; then
+        sudo -u ${quilibrium_user} /usr/bin/ln -sfr ${local_artifact_path} ${local_artifact_link}
+        if [ "${remote_artifact}" = "node-${latest_version}-linux-amd64" ]; then
+            sudo -u ${quilibrium_user} chmod +x ${local_artifact_path}
+            sudo semanage fcontext -a -t bin_t ${local_artifact_path} > /dev/null
+            sudo chcon -v -u system_u -t bin_t ${local_artifact_path} > /dev/null
+            sudo semanage fcontext -a -t bin_t ${local_artifact_link} > /dev/null
+            sudo chcon -v -u system_u -t bin_t ${local_artifact_link} > /dev/null
+        else
+            sudo semanage fcontext -a -t var_lib_t ${local_artifact_path} > /dev/null
+            sudo chcon -v -u system_u -t var_lib_t ${local_artifact_path} > /dev/null
+            sudo semanage fcontext -a -t var_lib_t ${local_artifact_link} > /dev/null
+            sudo chcon -v -u system_u -t var_lib_t ${local_artifact_link} > /dev/null
+        fi
+    elif sudo -u ${quilibrium_user} test -e ${local_artifact_path}; then
+        echo "auto-updater: observed corrupted file ${local_artifact_path}"
+        rm -f ${local_artifact_path}
+        rm -f ${local_artifact_link}
     else
-        semanage fcontext -a -t var_lib_t ${local_artifact_path}
-        chcon -v -u system_u -t var_lib_t ${local_artifact_path}
-        semanage fcontext -a -t var_lib_t ${local_artifact_link}
-        chcon -v -u system_u -t var_lib_t ${local_artifact_link}
+        echo "auto-updater: observed missing file ${local_artifact_path}"
+        rm -f ${local_artifact_link}
     fi
 done
+#sudo systemctl start quilibrium.service
