81deb19517b18a44182e0a344ce8a196713d35dd
rob thijssen <rthijssen@gmail.com>
2025-02-04 16:00:32 +0800

    volume mount syntax correction

--- script/encrypt-secrets.sh
+++ script/encrypt-secrets.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
 repo_path=${HOME}/git/grenade/rubberneck
-recipient=872E0C00FDC2E9CE9AD7509285F3DBFFC1A8E01B
 
 declare -a targets=()
-targets+=( ${repo_path}/manifest/dimitar-talev/quordlepleen/var/lib/quilibrium/.node/config.yml )
-targets+=( ${repo_path}/manifest/dimitar-talev/frootmig/etc/monero/monerod.conf )
-targets+=( ${repo_path}/manifest/dimitar-talev/mitko/etc/xmrig-proxy/config.json )
-targets+=( ${repo_path}/static/etc/xmrig/config.json )
-targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-qbtc-dev-ubuntu-rust.toml )
-targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-fedora-ci.toml )
+#targets+=( ${repo_path}/manifest/dimitar-talev/quordlepleen/var/lib/quilibrium/.node/config.yml )
+#targets+=( ${repo_path}/manifest/dimitar-talev/frootmig/etc/monero/monerod.conf )
+#targets+=( ${repo_path}/manifest/dimitar-talev/mitko/etc/xmrig-proxy/config.json )
+#targets+=( ${repo_path}/static/etc/xmrig/config.json )
+#targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-qbtc-dev-ubuntu-rust.toml )
+#targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-fedora-ci.toml )
 targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-fedora-infra.toml )
-targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-ubuntu-ci.toml )
+#targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-ubuntu-ci.toml )
 targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-ubuntu-infra.toml )
-targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-ubuntu-rust.toml )
+#targets+=( ${repo_path}/manifest/resonance/trillian/etc/gitlab-runner/config-resonance-ubuntu-rust.toml )
 
 for target in ${targets[@]}; do
     if [ -s ${target} ]; then
-        #case "${target}" in
-        #    ${repo_path}/manifest/resonance/*)
-        #        # resonance
-        #        recipient=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-        #        ;;
-        #    *)
-        #        # kavula
-        #        recipient=872E0C00FDC2E9CE9AD7509285F3DBFFC1A8E01B
-        #        ;;
-        #esac
+        case "${target}" in
+            ${repo_path}/manifest/resonance/*)
+                # resonance
+                recipient=5423ED2D79DA063B851A589CE0EBF17F94536628
+                ;;
+            *)
+                # kavula
+                recipient=872E0C00FDC2E9CE9AD7509285F3DBFFC1A8E01B
+                ;;
+        esac
         observed_sha256=$(sha256sum ${target} | cut -d ' ' -f 1 | tee ${target}.sha256)
         encrypted_sha256=$(cat ${target}.gpg.sha256 2> /dev/null)
         if [ -s ${target}.gpg ] && [ "${observed_sha256}" = "${encrypted_sha256}" ]; then
